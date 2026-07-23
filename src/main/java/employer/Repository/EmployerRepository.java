07a0eeb8fd5a97c75e02c09a2abe21840ee6f9a1
shoxruxdjamshidovich22-spec <shoxrux.djamshidovich22@gmail.com>
2026-07-23 14:44:50 +0500

    employer

---
+++ src/main/java/employer/Repository/EmployerRepository.java
@@ -0,0 +1,15 @@
+package employer.Repository;
+
+import employer.EmployerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployerRepository extends JpaRepository<EmployerEntity, Long> {
+
+    List<EmployerEntity> findByEmployerChatId(Long employerChatId);
+
+    List<EmployerEntity> findByStatus(String status);
+}
