854b91571e820cf4c2450774b2e0ee6714c4c4e3
Nodirbek Turaev <turaevn449@gmail.com>
2026-07-25 14:38:36 +0500

    admin panel added

--- src/main/java/uz/imaan/jobplatform/swagger/Swagger.java
+++ src/main/java/uz/imaan/jobplatform/swagger/Swagger.java
@@ -1,17 +1,1 @@
-package uz.imaan.jobplatform.swagger;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-
-
-public class Swagger {
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Card API")
-                        .version("1.0")
-                        .description("Simple Card Management API"));
-    }
-}
