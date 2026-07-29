2702cbf838ff1482d28bca4e815703f58b9c747d
ddavvronnn-del <ddavvronnn@gamil.com>
2026-07-29 15:29:28 +0500

    kategoriya tugma qoshildi

--- src/main/java/uz/imaan/jobplatform/telegram/Telegram.java
+++ src/main/java/uz/imaan/jobplatform/telegram/Telegram.java
@@ -9,7 +9,6 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import uz.imaan.jobplatform.employer.controller.EmployerController;
 import uz.imaan.jobplatform.employer.service.EmployerService;
 
 import java.util.ArrayList;
@@ -74,6 +73,13 @@ public class Telegram extends TelegramLongPollingBot {
             }
             else if (text.equals("Mening arizalarim")) { // <-- SHU QISMINI QO'SHING
                 sendMessage(chatId, "Siz topshirgan arizalar ro'yxati (tez orada ulanadi)...");
+            }
+            // 6. KATEGORIYA TUGMALARI BOSILGANDA
+            else if (text.equals("IT va Dasturlash") || text.equals("Dizayn") || text.equals("Marketing") || text.equals("Moliya va Hisob-kitob")) {
+                sendMessage(chatId, "Siz tanlagan '" + text + "' kategoriyasi bo'yicha vakansiyalar ro'yxati...");
+            }
+            else if (text.equals("Ortga")) {
+                sendJobSeekerMenu(chatId, "Siz Ish izlovchi menyusidasiz. Kerakli bo'limni tanlang:");
             }
         }
     }
@@ -145,6 +151,40 @@ public class Telegram extends TelegramLongPollingBot {
 
         keyboard.add(row1);
         keyboard.add(row2);
+        keyboardMarkup.setKeyboard(keyboard);
+        message.setReplyMarkup(keyboardMarkup);
+
+        executeMessage(message);
+    }
+
+    // Kategoriyalar Menyusi
+    private void sendCategoriesMenu(Long chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId.toString());
+        message.setText(text);
+
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setResizeKeyboard(true);
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // 1-qator
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add(new KeyboardButton("IT va Dasturlash"));
+        row1.add(new KeyboardButton("Dizayn"));
+
+        // 2-qator
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add(new KeyboardButton("Marketing"));
+        row2.add(new KeyboardButton("Moliya va Hisob-kitob"));
+
+        // 3-qator (Ortga qaytish)
+        KeyboardRow row3 = new KeyboardRow();
+        row3.add(new KeyboardButton("Ortga"));
+
+        keyboard.add(row1);
+        keyboard.add(row2);
+        keyboard.add(row3);
+
         keyboardMarkup.setKeyboard(keyboard);
         message.setReplyMarkup(keyboardMarkup);
 
