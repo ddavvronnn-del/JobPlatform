195af3c13a3b031ea1924cbf7c5dc477e3c6c799
shoxruxdjamshidovich22-spec <shoxrux.djamshidovich22@gmail.com>
2026-07-29 15:14:34 +0500

    emplyeer updated telegram BOT

--- src/main/java/uz/imaan/jobplatform/telegram/Telegram.java
+++ src/main/java/uz/imaan/jobplatform/telegram/Telegram.java
@@ -19,10 +19,11 @@ import java.util.List;
 @Component
 public class Telegram extends TelegramLongPollingBot {
 
-    private final String botUsername = "@JobPlatformUzBot";
-
-    public Telegram() {
+    private final String botUsername = "JobPlatformUzBot";
+    private final EmployerService employerService;
+    public Telegram(EmployerService employerService) {
         super("8449248126:AAHly6vbiHKNoCUhG_uc1EU2dfuO4DB6Ycg");
+        this.employerService = employerService;
     }
 
     @Override
@@ -54,7 +55,7 @@ public class Telegram extends TelegramLongPollingBot {
             // 4. EMPLOYER MENYU TUGMALARI ISHLASHI (Sizning serviceringiz chaqiriladi)
             else if (text.equals("Mening e'lonlarim")) {
                 // EmployerService dagi metodni chaqirib e'lonlarni olasiz
-                EmployerService employerService = null;
+
                 var jobs = employerService.getByEmployerChatId(chatId);
                 if (jobs.isEmpty()) {
                     sendMessage(chatId, "Sizda hali yaratilgan e'lonlar yo'q.");
@@ -163,6 +164,7 @@ public class Telegram extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
+
 }
 
 
