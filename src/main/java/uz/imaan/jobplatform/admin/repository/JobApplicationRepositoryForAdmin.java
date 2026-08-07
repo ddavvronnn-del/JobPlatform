81bdfd3f938b09abf24d4b587b5701e769722b42
Nodirbek Turaev <turaevn449@gmail.com>
2026-08-07 15:30:49 +0500

    everything updated

--- src/main/java/uz/imaan/jobplatform/admin/repository/JobApplicationRepositoryForAdmin.java
+++ src/main/java/uz/imaan/jobplatform/admin/repository/JobApplicationRepositoryForAdmin.java
@@ -2,11 +2,12 @@ package uz.imaan.jobplatform.admin.repository;
 
 
 
-import com.sun.research.ws.wadl.Application;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import uz.imaan.jobplatform.application.entity.ApplicationEntity;
 
 
 @Repository
-public interface JobApplicationRepositoryForAdmin extends JpaRepository<Application, Long> {
+public interface JobApplicationRepositoryForAdmin extends JpaRepository<ApplicationEntity, Long> {
 }
