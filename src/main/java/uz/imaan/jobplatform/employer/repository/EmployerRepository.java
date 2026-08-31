a8cdec3ea13504d38aa4bbbe4a0d96cef31d70ac
shoxruxdjamshidovich22-spec <shoxrux.djamshidovich22@gmail.com>
2026-08-31 15:35:23 +0500

    emplyeerHandler updated + -. m

--- src/main/java/uz/imaan/jobplatform/employer/repository/EmployerRepository.java
+++ src/main/java/uz/imaan/jobplatform/employer/repository/EmployerRepository.java
@@ -1,6 +1,6 @@
 package uz.imaan.jobplatform.employer.repository;
 
-import feign.Param;
+import org.springframework.data.repository.query.Param; // <-- To'g'ri import
 import org.springframework.data.jpa.repository.Query;
 import uz.imaan.jobplatform.employer.entity.EmployerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,8 +11,10 @@ import java.util.Optional;
 
 @Repository
 public interface EmployerRepository extends JpaRepository<EmployerEntity, Long> {
-    @Query("SELECT e FROM Employer e WHERE e.userId = :tgId OR e.id = :tgId")
+
+    @Query("SELECT e FROM EmployerEntity e WHERE e.employerChatId = :tgId OR e.id = :tgId")
     Optional<EmployerEntity> findByTgId(@Param("tgId") Long tgId);
+
     List<EmployerEntity> findByStatus(String status);
 
     Optional<EmployerEntity> findByEmployerChatId(Long employerChatId);
